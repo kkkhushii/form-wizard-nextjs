e75029020fcd4fc60f5e9a885103df44ac690222
kkkhushii <khushijoshi512@gmail.com>
2024-05-15 18:06:01 +0530

    "changes"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
 'use client'
 
 
-import UseAccountfirst from '../app/Component/UseAccountfirst' 
+import UseAccountfirst from '../app/Component/UseAccountfirst'
 import PersonalInfo from '../app/Component/PersonalInfo'
 import PaymentPage from '../app/Component/PaymentPage'
 import FinishPage from '../app/Component/FinishPage'
-import  '../app/Component/UserAcStyle.css'
+import '../app/Component/UserAcStyle.css'
 import { useState } from 'react';
-import ImageIcon from '../app/Component/ImageIcon'
 
 
 export default function Home() {
   const [step, setStep] = useState(1);
 
-  
+
   const handleNext = () => {
     setStep(step + 1);
   };
@@ -29,13 +28,13 @@ export default function Home() {
 
   return (
     <div className='h-screen flex justify-center items-center  custom-gradient'>
-    {/* <div className='h-screen flex justify-center items-center '> */}
+      {/* <div className='h-screen flex justify-center items-center '> */}
 
-       {step === 1 && <UseAccountfirst onNext={handleNext}  onSetImage={handleimage}  />}
-      {step === 2 && <PersonalInfo onPrevious={handlePrevious} onNext={handleNext}  onSetImage={handleimage}/>}
-      {step === 3 && <PaymentPage onPrevious={handlePrevious} onNext={handleNext}  onSetImage={handleimage}/>}
-      {step === 4 && <FinishPage onPrevious={handlePrevious}  onSetImage={handleimage}/>}  
+      {step === 1 && <UseAccountfirst onNext={handleNext} onSetImage={handleimage} />}
+      {step === 2 && <PersonalInfo onPrevious={handlePrevious} onNext={handleNext} onSetImage={handleimage} />}
+      {step === 3 && <PaymentPage onPrevious={handlePrevious} onNext={handleNext} onSetImage={handleimage} />}
+      {step === 4 && <FinishPage onPrevious={handlePrevious} onSetImage={handleimage} />}
     </div>
- 
+
   );
 }
